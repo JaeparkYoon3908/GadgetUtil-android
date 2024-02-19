86d9e07bbc82b20ffd9691cb0031fe4726cab244
YoonJaePark <yoonjaepark1122@gmail.com>
2024-02-19 22:21:10 +0900

    Display Utils 추가

--- gadget/gadget/src/main/kotlin/com/gadget/GadgetUtils.kt
+++ gadget/gadget/src/main/kotlin/com/gadget/GadgetUtils.kt
@@ -1,2 +1,21 @@
 package com.gadget
-object GadgetUtils
+
+object GadgetUtils {
+    val dayOfWeeksKoreaHashMap = hashMapOf(
+        "Mon" to "월요일",
+        "Tue" to "화요일",
+        "Wen" to "수요일",
+        "Wed" to "수요일",
+        "Thu" to "목요일",
+        "Fri" to "금요일",
+        "Sat" to "토요일",
+        "Sun" to "일요일",
+    )
+    val dayOfMonthHashMap = hashMapOf(
+        "Jan" to "01", "Feb" to "02", "Mar" to "03",
+        "Apr" to "04", "May" to "05", "Jun" to "06",
+        "Jul" to "07", "Aug" to "08", "Sep" to "09",
+        "Oct" to "10", "Nov" to "11", "Dec" to "12"
+    )
+}
+
