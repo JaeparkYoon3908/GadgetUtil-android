45fd9d2dbae6554871d61c6c1a882fb0294329e1
YoonJaepark <yoonjaepark1122@gmail.com>
2024-04-03 13:04:15 +0900

    Update build.gradle.kts

--- gadget/build.gradle.kts
+++ gadget/build.gradle.kts
@@ -48,14 +48,13 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("release") {
-                groupId = "com.github.YoonJaePark3908"
-                artifactId = "GadgetUtil-android"
-                version = "0.0.8"
-            }
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            groupId = "com.github.YoonJaePark3908"
+            artifactId = "GadgetUtil-android"
+            version = "0.0.8"
         }
     }
 }
